c07ab573ec1ee02e6196e5df83d9210af1fae317
Deepesh <deepesh259nitk@gmail.com>
2018-12-30 21:26:24 +0000

    Updated example project with script changes folder changes.

---
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by ITRMG on 2018-30-12.
+//  Copyright © 2018 djrecker. All rights reserved.
+//
+
+import UIKit
+import MixedFW
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
---
+++ ExampleApp/Pods/Target Support Files/MixedFW/MixedFW-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MixedFW : NSObject
+@end
+@implementation PodsDummy_MixedFW
+@end
