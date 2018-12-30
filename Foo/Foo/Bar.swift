d86091e62c30004a8dc0b608972a7209a0a64b0b
Deepesh <deepesh259nitk@gmail.com>
2018-12-30 11:33:33 +0000

    Contains custom framework with universal script to create a .framework file

---
+++ Foo/Foo/Bar.swift
@@ -0,0 +1,27 @@
+//
+//  Bar.swift
+//  Foo
+//
+//  Created by Daniel Eggert on 09/01/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+import Foo_Private
+
+@objc open class Bar: NSObject {
+   
+    @objc open func doSomething() {
+        // We can access Baz, which is public:
+        let b = Baz()
+        print("test B")
+        b.doSomething()
+        
+        // We can also access Norf, even though it is not public:
+        let n = Norf()
+        print("test N")
+        
+        let q = Qux()
+        print("test Q")
+    }
+}
---
+++ Foo/Foo/Baz.m
@@ -0,0 +1,28 @@
+//
+//  Baz.m
+//  Foo
+//
+//  Created by Daniel Eggert on 09/01/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+#import "Baz.h"
+
+#import "Norf.h"
+
+// It's a bit off, but apparently the only way we can access Swift is through a header that lives inside the framework's public "Headers" directory.
+#import <Foo/Foo-Swift.h>
+
+
+
+@implementation Baz
+
+- (void)doSomething;
+{
+    NSLog(@"Baz ---- test ");
+    [[[Bar alloc] init] doSomething];
+    NSLog(@"%@", [[Norf alloc] init]);
+    NSLog(@"Baz ---- objc end ");
+}
+
+@end
